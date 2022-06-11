4cf13306d8b4d3ab75caf6d414bb05dacb1d2847
Bompally Harish <harishfebblar@gmail.com>
2022-06-11 16:59:00 +0530

    pulled a code from we dev simplified channel

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
-require("dotenv").config()
-
 const express = require("express")
-const app = express()
+require("dotenv").config()
 const cors = require("cors")
+const app = express()
 app.use(express.json())
 app.use(
   cors({
